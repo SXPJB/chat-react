fc4837d66f090566da3bd9c1fcd456d2847dc9ef
SXPJB <emmanuel.hernandezramirez@gmail.com>
2022-08-27 12:24:54 -0500

    [SXPJB] final result v2

--- server/index.js
+++ server/index.js
@@ -13,13 +13,38 @@ const io = new SocketServer(server, {
     }
 })
 
+const users = []
+
 const onConnection = socket => {
     console.log("New user connected", socket.id)
+    socket.on('login', user => {
+        if(!users.find(u => u.nickname === user.nickname)){
+            const tmpUser = {
+                id: socket.id,
+                ...user
+            }
+            users.push(tmpUser)
+            socket.emit('login',tmpUser)
+            console.log("User login ",tmpUser.nickname)
+        } else {
+            socket.emit('userFound', user.nickname + ' username is taken! Try some other username.')
+        }
+    })
+
+    socket.on('logout',({id})=>{
+        console.log('logout',id)
+        const userRemoved = users.find(u=>u.id === id)
+        if(userRemoved){
+            users.slice(users.findIndex(u=>u.id===userRemoved.id),1)
+            socket.emit('logout',userRemoved)
+            console.log('User logout',userRemoved.nickname)
+        }
+    })
+
     socket.on('message', message => {
-        console.log(message)
-        socket.broadcast.emit('message',{
+        socket.broadcast.emit('message', {
             ...message,
-            from:'Anonymous'
+            from: users.find(user => user.id === socket.id).nickname
         })
     })
 }
